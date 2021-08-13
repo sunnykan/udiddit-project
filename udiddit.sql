6d3e537998bb352f78d53948d129a17307202a61
K S <8283247+sunnykan@users.noreply.github.com>
2021-08-13 04:41:19 -0400

    Made suggested changes from review except for index on vote

--- udiddit.sql
+++ udiddit.sql
@@ -2,44 +2,54 @@ DROP TABLE IF EXISTS user_account;
 
 CREATE TABLE user_account(
     id SERIAL,
-    username VARCHAR(25) NOT NULL,
+    username VARCHAR(25),
+    last_login TIMESTAMP WITH TIME ZONE,
     UNIQUE(username),
-    CONSTRAINT user_account_pkey PRIMARY KEY(id)
+    CONSTRAINT username_nonnull_nonempty
+        CHECK (NULLIF(TRIM(username), '') IS NOT NULL),
+    CONSTRAINT user_account_pkey 
+        PRIMARY KEY(id)
 );
-
--- The user name cannot be empty.
-ALTER TABLE user_account
-ADD CONSTRAINT username_non_empty
-CHECK (NULLIF(TRIM(username), '') IS NOT NULL);
 
 DROP TABLE IF EXISTS topic;
 
 CREATE TABLE topic(
     id SERIAL,
     user_id INTEGER,
-    name VARCHAR(30) NOT NULL,
-    description VARCHAR(500),
-    UNIQUE(name),
-    CONSTRAINT topic_pkey PRIMARY KEY(id),
+    topic_name VARCHAR(30),
+    topic_description VARCHAR(500),
+    created_at TIMESTAMP WITH TIME ZONE,
+    UNIQUE(topic_name),
+    CONSTRAINT topic_title_nonnull_nonempty
+        CHECK (NULLIF(TRIM(topic_name), '') IS NOT NULL),
+    CONSTRAINT topic_pkey 
+        PRIMARY KEY(id),
     CONSTRAINT fk_user
         FOREIGN KEY(user_id)
             REFERENCES user_account(id)
 );
 
--- The topic's name cannot be empty.
-ALTER TABLE topic
-ADD CONSTRAINT topic_title_non_empty
-CHECK (NULLIF(TRIM(name), '') IS NOT NULL);
-
 DROP TABLE IF EXISTS post;
 
 CREATE TABLE post(
-    id INTEGER NOT NULL,
-    topic_id INTEGER,
+    id INTEGER,
+    topic_id INTEGER NOT NULL,
     user_id INTEGER,
-    title VARCHAR(100) NOT NULL,
+    title VARCHAR(100),
     url TEXT,
     text_content TEXT,
+    created_at TIMESTAMP WITH TIME ZONE,
+    CONSTRAINT post_title_nonnull_nonempty
+        CHECK (NULLIF(TRIM(title), '') IS NOT NULL),
+    CONSTRAINT url_text_context_one_nonempty 
+        CHECK (
+                (NULLIF(TRIM(url), '') IS NULL 
+                    OR NULLIF(TRIM(text_content), '') IS NULL) 
+                AND NOT (NULLIF(TRIM(url), '') IS NULL 
+                    AND NULLIF(TRIM(text_content), '') IS NULL)
+        ),
+    CONSTRAINT post_pkey 
+        PRIMARY KEY(id),
     CONSTRAINT fk_topic
         FOREIGN KEY(topic_id)
             REFERENCES topic(id)
@@ -50,36 +60,54 @@ CREATE TABLE post(
             ON DELETE SET NULL
 );
 
--- The title of a post cannot be empty.
-ALTER TABLE post
-ADD CONSTRAINT post_title_non_empty
-CHECK (NULLIF(TRIM(title), '') IS NOT NULL);
+DROP TABLE IF EXISTS vote;
 
--- Posts should contain either a URL or a text content, but not both.
-ALTER TABLE post
-ADD CONSTRAINT url_text_context_one_nonempty
-CHECK (
-    (NULLIF(TRIM(url), '') IS NULL 
-        OR NULLIF(TRIM(text_content), '') IS NULL) 
-    AND NOT (NULLIF(TRIM(url), '') IS NULL 
-        AND NULLIF(TRIM(text_content), '') IS NULL)
+CREATE TABLE vote(
+    id SERIAL,
+    post_id INTEGER NOT NULL,
+    user_id INTEGER,
+    vote SMALLINT NOT NULL,
+    CONSTRAINT one_vote_one_user 
+        UNIQUE(post_id, user_id),
+    CONSTRAINT vote_choice
+        CHECK (vote in (1, -1)),
+    CONSTRAINT vote_pkey PRIMARY KEY(id),   
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES user_account(id)
+            ON DELETE SET NULL,
+    CONSTRAINT fk_post
+        FOREIGN KEY(post_id)
+            REFERENCES post(id)
+            ON DELETE CASCADE
 );
 
--- trim fields in bad_posts and bad_comments table
--- based on field length constraints
--- will this introduce duplication?
--- why not do it on insertion?
-UPDATE bad_posts 
-SET username=LEFT(username, 25);
+DROP TABLE IF EXISTS comment;
 
-UPDATE bad_posts
-SET topic=LEFT(topic, 30);
-
-UPDATE bad_posts
-SET title=LEFT(title, 100);
-
-UPDATE bad_comments
-SET username=LEFT(username, 25);
+CREATE TABLE comment(
+    id INTEGER,
+    post_id INTEGER NOT NULL,
+    user_id INTEGER,
+    parent_id INTEGER, -- should be NULL but we don't have data
+    text_content TEXT,
+    created_at TIMESTAMP WITH TIME ZONE,
+    CONSTRAINT comment_text_content_non_empty
+        CHECK (NULLIF(TRIM(text_content), '') IS NOT NULL),
+    CONSTRAINT comment_pkey 
+        PRIMARY KEY(id),
+    CONSTRAINT fk_post
+        FOREIGN KEY(post_id)
+            REFERENCES post(id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES user_account(id)
+            ON DELETE SET NULL,
+    CONSTRAINT fk_comment
+        FOREIGN KEY(parent_id)
+            REFERENCES comment(id)
+            ON DELETE CASCADE
+);
 
 -- create a votes view - to be dropped at the end
 CREATE VIEW vote_view 
@@ -96,25 +124,19 @@ FROM bad_posts;
 
 -- populate user_account table
 INSERT INTO user_account(username)
-(SELECT DISTINCT(username) 
+(SELECT DISTINCT(username::VARCHAR(25)) 
 FROM bad_posts
 UNION
-SELECT DISTINCT(username)
+SELECT DISTINCT(username::VARCHAR(25))
 FROM bad_comments
-);
-
--- account for the users in the votes table 
-INSERT INTO user_account(username)
-(SELECT username
+UNION
+SELECT DISTINCT(username::VARCHAR(25))
 FROM vote_view
-EXCEPT
-SELECT username
-FROM user_account
 );
 
 -- populate topic table
-INSERT INTO topic(name)
-SELECT bp.topic
+INSERT INTO topic(topic_name)
+SELECT bp.topic::VARCHAR(30)
 FROM bad_posts bp
 JOIN user_account ua
 ON bp.username = ua.username
@@ -125,37 +147,14 @@ INSERT INTO post(id, topic_id, user_id, title, url, text_content)
 SELECT bp.id id, 
     t.id topic_id, 
     ua.id user_id,
-    bp.title title, 
+    bp.title::VARCHAR(100) title, 
     bp.url url, 
     bp.text_content text_content
 FROM bad_posts bp
 JOIN user_account ua
 ON bp.username = ua.username
 JOIN topic t
-ON bp.topic = t.name;
-
--- set primary key for post table
-ALTER TABLE post 
-ADD CONSTRAINT post_pkey 
-PRIMARY KEY(id);
-
-DROP TABLE IF EXISTS vote;
-
-CREATE TABLE vote(
-    id SERIAL,
-    post_id INTEGER,
-    user_id INTEGER,
-    vote SMALLINT,
-    CONSTRAINT vote_pkey PRIMARY KEY(id),   
-    CONSTRAINT fk_user
-        FOREIGN KEY(user_id)
-            REFERENCES user_account(id)
-            ON DELETE SET NULL,
-    CONSTRAINT fk_post
-        FOREIGN KEY(post_id)
-            REFERENCES post(id)
-            ON DELETE CASCADE
-);
+ON bp.topic = t.topic_name;
 
 -- Populate table vote
 INSERT INTO vote(post_id, user_id, vote)
@@ -165,41 +164,6 @@ SELECT vw.post_id post_id,
 FROM vote_view vw
 JOIN user_account ua
 ON vw.username = ua.username;
-
-ALTER TABLE vote
-ADD CONSTRAINT one_vote_one_user 
-UNIQUE(post_id, user_id);
-
-ALTER TABLE vote
-ADD CONSTRAINT vote_choice
-CHECK (vote in (1, -1));
-
-DROP TABLE IF EXISTS comment;
-
-CREATE TABLE comment(
-    id INTEGER NOT NULL,
-    post_id INTEGER,
-    user_id INTEGER,
-    text_content TEXT NOT NULL,
-    parent_id INTEGER,
-    CONSTRAINT fk_post
-        FOREIGN KEY(post_id)
-            REFERENCES post(id)
-            ON DELETE CASCADE,
-    CONSTRAINT fk_user
-        FOREIGN KEY(user_id)
-            REFERENCES user_account(id)
-            ON DELETE SET NULL,
-    CONSTRAINT fk_comment
-        FOREIGN KEY(parent_id)
-            REFERENCES comment(id)
-            ON DELETE CASCADE
-);
-
-
-ALTER TABLE comment
-ADD CONSTRAINT comment_text_content_non_empty
-CHECK (NULLIF(TRIM(text_content), '') IS NOT NULL);
 
 -- Populate table comment
 INSERT INTO comment(id, post_id, user_id, text_content)
@@ -211,9 +175,29 @@ FROM user_account ua
 JOIN bad_comments bc
 ON ua.username = bc.username;
 
--- set primary key for comment table
-ALTER TABLE comment 
-ADD CONSTRAINT comment_pkey 
-PRIMARY KEY(id);
+-- indices
+-- table: user
+CREATE INDEX index_last_login_user ON user_account(last_login);
 
+-- table: topic
+CREATE INDEX index_fk_user_topic ON topic(user_id);
+CREATE INDEX index_created_at_topic ON topic(created_at);
+
+-- table: post
+CREATE INDEX index_fk_topic_post ON post(topic_id);
+CREATE INDEX index_fk_user_post ON post(user_id);
+CREATE INDEX index_created_at_post ON post(created_at);
+CREATE INDEX index_url_post ON post(url);
+
+-- table: vote
+CREATE INDEX index_fk_post_vote ON vote(post_id);
+CREATE INDEX index_fk_user_vote ON vote(user_id);
+
+-- table: comment
+CREATE INDEX index_fk_post_comment ON comment(post_id);
+CREATE INDEX index_fk_user_comment ON comment(user_id);
+CREATE INDEX index_fk_parent_comment ON comment(parent_id);
+CREATE INDEX index_created_at_comment ON comment(created_at);
+
+-- drop view
 DROP VIEW vote_view;
